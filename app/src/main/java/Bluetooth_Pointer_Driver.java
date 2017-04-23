4ef485ca5add934850e630461fee291cb36f1da1
Phillip Porter <phillip.porter54936@outlook.com>
2017-04-23 02:50:44 -0700

    Added structure, and basic layout

---
+++ app/src/main/java/Bluetooth_Pointer_Driver.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Phillip on 4/23/2017.
+ */
+
+import java.util.Set;
+import java.util.ArrayList;
+import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.content.Intent;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
+public class Bluetooth_Pointer_Driver
+{
+    // Setup Bluetooth
+    BluetoothAdapter Ba = BluetoothAdapter.getDefaultAdapter();
+
+    
+
+}
---
+++ app/src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Phillip on 4/23/2017.
+ */
+
+public class Main
+{
+
+}
